2c683dcfc8c6211f8ebed2d91b530c28a146acf1
LRWyatt801 <loganwyatt1995@gmail.com>
2025-02-08 12:15:03 -0600

    Init repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sorting Algorithms Advanced
+
+In this repo I'm learning and practicing with more sorting algorithms. <br>
+The algorithms I'll be working on are
+- Shell Sort (Knuth Sequence)
+- Couting Sort
+- Merge Sort
---
+++ sorting.h
@@ -0,0 +1,12 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+#include <stdio.h>
+
+/***** PROTOTYPES *****/
+
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
+#endif /* SORTING_H */
