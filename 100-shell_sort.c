5301364ba8e591da7b9bda4d66359ba07d60eabb
LRWyatt801 <loganwyatt1995@gmail.com>
2025-02-11 20:34:12 -0600

    Fix betty.  Prints are still wrong

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+* shell_sort - sort an int array using knuth sequence
+* @array: int array to sort
+* @size: size of array
+*
+* Return: n/a
+*/
+
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1;
@@ -15,13 +23,22 @@ void shell_sort(int *array, size_t size)
 	{
 		for (i = gap; i < size; i++)
 		{
-			for ( j = i; j >= gap && array[j] < array[j-gap]; j -= gap)
-				swap(&array[j], &array[j-gap]);
+			for (j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
+				swap(&array[j], &array[j - gap]);
+
 		}
-		print_array(array, size);
 		gap /= 3;
+		print_array(array, size);
 	}
 }
+
+/**
+* swap - swaps values in an array at spots a and b
+* @a: value in array
+* @b: value in array
+*
+* Return: n/a
+*/
 
 void swap(int *a, int *b)
 {
--- print_array.c
+++ print_array.c
@@ -1,8 +1,17 @@
 #include "sort.h"
 
+/**
+* print_array - prints an array
+* @array: array to print
+* @size: size of array
+*
+* Return: n/a
+*/
+
 void print_array(int *array, size_t size)
 {
 	size_t i;
+
 	for (i = 0; i < size; i++)
 	{
 		printf("%d", array[i]);
